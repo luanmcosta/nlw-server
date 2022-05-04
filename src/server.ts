30f0feba29450683d4ad145d07a6946d749af677
Luan Mineu Costa <luanwebdev@gmail.com>
2022-05-04 19:15:29 -0300

    created test cases and updated submit feedback use case

--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,13 @@
 import express from 'express'
+import cors from 'cors'
 import { routes } from './routes';
 
 const app = express();
 
-
-app.use(express.json())
+app.use(cors());
+app.use(express.json());
 app.use(routes);
 
 app.listen(3333, () => {
-    console.log('HTTP Server running!')
+    console.log('HTTP Server running!');
 });
--- src/use-cases/submit-feedback-use-case.spec.ts
+++ src/use-cases/submit-feedback-use-case.spec.ts
@@ -1,3 +1,54 @@
-test('sum 2+ 2', () => {
-    expect(2+2).toBe(4)
-})
+import { SubmitFeedbackUseCase } from "./submit-feedback-use-case";
+
+const createFeedbackSpy = jest.fn();
+const sendMailSpy = jest.fn();
+
+describe('Submit feedback', () => {
+
+    const submitFeedback = new SubmitFeedbackUseCase(
+        { create: createFeedbackSpy }, 
+        { sendMail: sendMailSpy }
+    )
+
+    it('should be able to submit a feedback', async () => {
+        
+        await expect(submitFeedback.execute({
+            type: 'BUG',
+            comment: 'example comment',
+            screenshot: 'data:image/png;base64.awdawdawdawwd'
+        })).resolves.not.toThrow();
+
+        expect(createFeedbackSpy).toHaveBeenCalled();
+        expect(sendMailSpy).toHaveBeenCalled();
+
+    });
+
+  
+
+    it('should not be able to submit a feedback without type', async () => {
+        
+        await expect(submitFeedback.execute({
+            type: '',
+            comment: 'example comment',
+            screenshot: 'data:image/png;base64.awdawdawdawwd'
+        })).rejects.toThrow();
+    });
+
+    it('should not be able to submit a feedback without a comment', async () => {
+        
+        await expect(submitFeedback.execute({
+            type: 'Bug',
+            comment: '',
+            screenshot: 'data:image/png;base64.awdawdawdawwd'
+        })).rejects.toThrow();
+    });
+
+    it('should not be able to submit a feedback with invalid screenshot format', async () => {
+        
+        await expect(submitFeedback.execute({
+            type: 'BUG',
+            comment: 'example comment',
+            screenshot: 'wrong.png'
+        })).rejects.toThrow();
+    });
+});
